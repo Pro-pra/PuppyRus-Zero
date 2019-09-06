fdca40ecf15b8b786aeff34aacdf81dbaf2811c6
Pro-pra <pro2_2000@mail.ru>
2019-09-06 14:43:41 +0900

    Update makeiso.sh

--- makeiso.sh
+++ makeiso.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-ISODIR=isolinux-builds/ #каталог с файлами для упаковки в iso
-OUTPUT=/mnt/sda1/ #каталог в котором создавать iso
-DATE=`date +%y.%m`  #подстановка текущей даты в имя образа
+mkisofs="`which mkisofs || which genisoimage`"
+[ "$mkisofs" = "" ] && echo "Не найдена программа для создания iso" &&exit
 
-#/usr/bin/mkisofs -allow-lowercase -J -D -R -o "$OUTPUT"PuppyRus-base-"$DATE".iso -m "$ISODIR"'packages/*' -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table "$ISODIR"
-#/usr/bin/mkisofs -allow-lowercase -J -D -R -o "$OUTPUT"PuppyRus-base-"$DATE"-xorg.iso -m "$ISODIR"'packages/puppyrus*.pfs' -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table "$ISODIR"
-/usr/bin/mkisofs -allow-lowercase -J -D -R -o "$OUTPUT"PuppyRus-Icewm-"$DATE".iso -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table "$ISODIR"
-#/usr/bin/mkisofs -allow-lowercase -J -D -R -o "$OUTPUT"PuppyRus-Icewm-"$DATE"-Big.iso -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table "$ISODIR"
+[ "$1" ] && ISODIR="$1" || ISODIR=isolinux-builds #каталог с файлами для упаковки в iso
+
+[ ! -d "$ISODIR" ] && echo "Не найден каталог в котором находятся файлы для iso" && exit
+cd "$ISODIR"
+OUTPUT=../ #каталог в котором создавать iso
+DATE=`date +%y%m%d`  #подстановка текущей даты в имя образа
+CDLABEL=Zero
+
+#$mkisofs -allow-lowercase -J -D -R -o "$OUTPUT"PuppyRus-Icewm-"$DATE".iso -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table "$ISODIR"
+$mkisofs -allow-lowercase -J -D -R -A "$CDLABEL" -V "$CDLABEL" -b grldr -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table -o "$OUTPUT"PuppyRus-Zero-"$DATE".iso .
 
 #секция удаления старых версий iso (например старее 1 года)
 
