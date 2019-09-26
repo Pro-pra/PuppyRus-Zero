246546df5286493431e9759f1fbf5cb0cc3df006
Pro-pra <pro2_2000@mail.ru>
2019-09-26 13:52:46 +0900

    Update dep-find.sh

--- dep-find.sh
+++ dep-find.sh
@@ -1,11 +1,30 @@
 #!/bin/ash
 #180109 sfs DDSE
 #190923 rewrite for Zero
+#190926 find repo package with needed lib
 [ ! "$1" ] && echo "Использование: $0 [-base] dir
     Поиск недостающих библиотек для модуля (пакета) распакованного в dir
     если не задан параметр -base поиск выполняется в существующем /usr/lib
     -base - поиск библиотек выполняется только в модуле base" && exit
 #[ "$1" = "-p" ] && p=1 && shift
+
+find_pkg () {
+[ "$1" ] || return
+#подключиться к серверу и выкачать MANIFEST.bz2
+local REPO="http://mirror.yandex.ru/slackware/slackware-current/slackware"
+#REPO="http://slackware.cs.utah.edu/pub/slackware/slackware-current/slackware"
+ 
+#получить список из репозитария slackware, записать в файл
+[ -f /var/tmp/MANIFEST.bz2 ] || curl $REPO/MANIFEST.bz2 > /var/tmp/MANIFEST.bz2
+#найти в содержимом нужный пакет, имя библиотеки передается в параметре функции
+[ -f /var/tmp/MANIFEST.bz2 ] && local PKG="`bzgrep --ignore-case --max-count=1 "Package:\|$1" /var/tmp/MANIFEST.bz2 | grep -B1 $1 | awk '/\.txz$/ {print $3}'| cut -b2-`" || return
+#скачать пакет
+if [ $PKG ]; then 
+    echo "Найдено в пакете $PKG"
+    curl ${REPO}${PKG} --output . || return
+    #wget ${REPO}${PKG} || return
+fi
+}
 
 
 if [ "$1" = "-base" ] ;then
@@ -29,7 +48,15 @@ if [ "$1" = "-base" ] ;then
 	:
 	else
 	    echo "$LIB не найден"
-	    find "$2" \( -type l -or -type f \) -name "$LIB" -exec echo "$LIB  найден в '$2'" \;
+	    if [ `find "$2" \( -type l -or -type f \) -name "$LIB"` ]; then
+		echo "$LIB  найден в '$2'"
+	    else
+		echo "Выполнить поиск пакета содержащего $LIB"
+		echo "в репозитарии Slackware?"
+		echo -n "Нажмите ENTER для пропуска, или любую букву для поиска."
+		read N
+		[ $N != "" ] && find_pkg $LIB
+	    fi
 	fi
     done
     exit
@@ -39,10 +66,15 @@ d="`find "$1" -type f -executable -exec ldd {} \; |awk '/=> not found/ {print $1
 #echo -n "cp -d "
 #echo "$d" |awk -F '\\.so' '{print $1".*"}'|tr "\n" " "
 echo
-echo ======== не хватает `echo $d|wc -w` библиотек ======
-echo "$d"
+echo "$d не найден"
 if [ "$d" ]; then
-echo
-echo "Проверка в содержимом '$1':"
-find "$1" \( -type l -or -type f \) -name "$d" -exec echo "$d  найден" \;
+    if [ `find "$1" \( -type l -or -type f \) -name "$d"` ]; then
+	echo "$d  найден в '$1'"
+    else
+	echo "Выполнить поиск пакета содержащего $d"
+	echo "в репозитарии Slackware?"
+	echo -n "Нажмите ENTER для пропуска, или любую букву для поиска."
+	read N
+	[ $N != "" ] && find_pkg $d
+    fi
 fi
