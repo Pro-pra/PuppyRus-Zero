942796507f2aa5cf75b3cee3647121673e647728
Pro-pra <pro2_2000@mail.ru>
2019-10-09 10:08:27 +0900

    Update makeiso.sh

--- makeiso.sh
+++ makeiso.sh
@@ -16,13 +16,15 @@ if [ -f ./grub2.eltorito ]; then
 else
   [ -f /usr/lib/grub/grub2.eltorito ] && cp -f /usr/lib/grub/grub2.eltorito ./ || echo "Не найден файл загрузчика grub2.eltorito"
 fi
-  
+#/usr/lib/grub/efi.img это образ раздела fat32 (можно монтировать) на котором размещен grub2
+#этот образ используется при загрузке с флешки через EFI. 
 
 $mkisofs  -as mkisofs -allow-lowercase -J -D -R -A "$CDLABEL" -V "$CDLABEL" \
 -no-emul-boot -boot-load-size 4 -hide boot.catalog -boot-info-table \
 -b grub2.eltorito --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
 -boot-info-table --grub2-boot-info grub2.eltorito \
--eltorito-alt-boot --efi-boot EFI/BOOT/bootx64.efi \
+-append-partition 1 0xef /usr/lib/grub/efi.img \
+-eltorito-alt-boot -e --interval:appended-partition_1:all:: \
 -no-emul-boot \
 -o "$OUTPUT"PuppyRus-Zero-"$DATE".iso  .
 
