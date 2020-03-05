5efdbac71b23845e5ada416c40c3fd99ec0aae22
Pro-pra <pro2_2000@mail.ru>
2020-03-05 16:49:15 +0900

    add --help option

--- grub-mkimage.sh
+++ grub-mkimage.sh
@@ -1,19 +1,27 @@
-#скрипт создает образы GRUB2 для использования на загрузочных носителях
-#Пересоздавать образы постоянно требуется только при обновлении GRUB2 или при
-#добавлении-удалении модулей в GRUB2.
-#Готовые сгенерированные образы находятся в каталоге /usr/lib/grub
+#!/bin/sh
 
-#Образы будут созданы в текущем каталоге, если все не требуется, закоментируйте ненужное
-#grubx64.efi - EFI образ
-#grub2 - El Torito образ для ISO
-
-#core.img - образ для загрузочного USB, используется вместе с boot.img
-#boot.img (из grub2) и core.img (делается grub-mkimage) записываются в MBR флешки или диска 
-#командой grub-bios-setup или dd, примеры (/dev/sdb заменить на свою флешку):
-#grub-bios-setup -d. -b /usr/lib/grub/i386-pc/boot.img -c ./core.img /dev/sdb
-#или
-#dd if=./core.img of=/dev/sdb bs=512 seek=1
-#dd if=/usr/lib/grub/i386-pc/boot.img of=/deb/sdb bs=446 count=1
+if [ "$1" = "--help" ]; then 
+echo "Cкрипт создает образы GRUB2 для использования на загрузочных носителях
+Пересоздавать образы требуется только при обновлении GRUB2 или при
+добавлении-удалении модулей в GRUB2.
+Готовые сгенерированные образы находятся в каталоге /usr/lib/grub"
+echo
+echo "Образы будут созданы в текущем каталоге, если все не требуется, 
+закоментируйте ненужную команду. 
+grubx64.efi - EFI образ 
+grub2.eltorito - El Torito образ для ISO"
+echo
+echo "core.img - образ для загрузочного USB, используется вместе с boot.img
+boot.img (из grub2) и core.img (делается grub-mkimage) записываются в MBR флешки
+или диска командой grub-bios-setup или dd, примеры (/dev/sdb заменить на свою флешку):"
+echo "grub-bios-setup -d. -b /usr/lib/grub/i386-pc/boot.img -c ./core.img /dev/sdb"
+echo
+echo "или"
+echo
+echo "dd if=./core.img of=/dev/sdb bs=512 seek=1
+dd if=/usr/lib/grub/i386-pc/boot.img of=/deb/sdb bs=446 count=1"
+exit
+fi
 
 # Для GPT загрузки (можно использовать при загрузке с HDD и ISO)
 grub-mkimage --directory=/usr/lib/grub/x86_64-efi --compression=auto \
@@ -23,7 +31,7 @@ part_gpt part_msdos disk memdisk fat exfat lvm ext2 ntfs iso9660 normal gzio xzi
 
 #для CDROM
 grub-mkimage --directory=/usr/lib/grub/i386-pc --compression=auto \
---config=/usr/share/grub/grub.cfg --verbose --prefix=/EFI/BOOT \
+--config=/usr/share/grub/grub_eltorito.cfg --verbose --prefix=/EFI/BOOT \
 --output=grub2.eltorito --format=i386-pc-eltorito \
 part_msdos biosdisk disk fat iso9660 test vbe vga normal gzio xzio search configfile linux linux16 chain loopback echo file halt reboot ls true gfxterm gettext font
 
@@ -31,5 +39,5 @@ part_msdos biosdisk disk fat iso9660 test vbe vga normal gzio xzio search config
 #grub-mkimage --directory=/usr/lib/grub/i386-pc --compression=none \
 #--verbose --prefix=\(hd0,msdos1\)/EFI/BOOT \
 #--output=core.img --format=i386-pc \
-#part_gpt part_msdos biosdisk disk memdisk fat lvm ntldr exfat ext2 ntfs iso9660 gzio xzio test vbe vga normal search configfile linux linux16 chain loopback echo file halt reboot ls true gfxterm gettext font
+#part_gpt part_msdos biosdisk disk memdisk fat ntldr lvm exfat ext2 ntfs iso9660 gzio xzio test vbe vga normal search configfile linux linux16 chain loopback echo file halt reboot ls true gfxterm gettext font
 
